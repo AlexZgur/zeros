c004ab40b84d1d40e04ea6f059308952c62d9e1f
AlexZgur <rugzla@ukr.net>
2019-10-10 01:03:06 +0300

    Add solution for zeros function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
   // your solution
+  let zCount = 0;
+  let c2 = 0, c5 = 0;
+  let parts = expression.split('*');  
+  for (let i = 0; i < parts.length; i++) {
+    let multiplier = parts[i].replace('!', '');
+    let each = multiplier.indexOf('!') < 0 ? true : false;
+    if (!each) multiplier = multiplier.replace('!', ''); 
+    let number = parseInt(multiplier);
+    let step = each ? 1 : 2;
+    for (let i = number; i > 0; i -= step) {
+      let buf = i;
+      while (buf % 5 == 0 && buf > 0) {
+        c5++;
+        buf /= 5;
+      }
+      buf = i;
+      while (buf % 2 == 0 && buf > 0) {
+        c2++;
+        buf /= 2;
+      }
+    }
+  }
+  zCount = Math.min(c2, c5);
+  return zCount;
 }
